52607d40e7b38b397cb4172fac65a7d62d0ecc3c
SparkMaster <chrisbarwick23@hotmail.com>
2012-10-05 09:37:46 -0500

    Added 3 new foods. :D Also added CHANGES.md Please check that instead of commits for more info

--- hybridcraft/FoodStuff/HybridModFoodStuff.java
+++ hybridcraft/FoodStuff/HybridModFoodStuff.java
@@ -1,19 +1,43 @@
 package hybridcraft.FoodStuff;
 
+import hybridcraft.IngotStuff.CommonProxyHybrid;
+import net.minecraft.src.Block;
+import net.minecraft.src.BlockWorkbench;
+import net.minecraft.src.Item;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = "HybridCraft 2 Food", name = "HybridCraft 2 Food", version = "2.2 beta 1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
-
 public class HybridModFoodStuff {
 
-	@Init
-	public void load(FMLInitializationEvent event)
-	{
-		
-	}
+	@SidedProxy(clientSide = "hybrid.client.ClientProxyHybrid", serverSide = "hybridcraft.IngotStuff.CommonProxyHybrid")
+	public static CommonProxyHybrid proxy;
 	
+	public static Item BaB;
+	public static Item PaB;
+	public static Item CaB;
+	// public static Block foodwb;
+
+	
+	@Init
+	public void load(FMLInitializationEvent event) {
+
+		// Classifications
+		BaB = new ItemBaBFood(662, 8, false).setIconIndex(0).setItemName("bab");
+		// foodwb = new FoodWorkbench(663).setHardness(2.5F).setStepSound(Block.soundWoodFootstep).setBlockName("foodwb");
+		PaB = new ItemPaBFood(664, 10, false).setIconIndex(1).setItemName("pab");
+		CaB = new ItemCaBFood(665, 10, false).setIconIndex(2).setItemName("cab");
+		
+		// Registry
+		LanguageRegistry.addName(BaB, "Beef and Bread");
+		LanguageRegistry.addName(PaB, "Pork and Bread");
+		LanguageRegistry.addName(CaB, "Chicken and Bread");
+		// LanguageRegistry.addName(foodwb, "Food Workbench [WIP]");
+	}
+
 }
