281371fb6a085c197c3962e5e2091b42e26ae40d
SparkMaster <chrisbarwick23@hotmail.com>
2012-10-05 01:05:48 -0500

    Setting up workspace for tomorrow. :) Im hungry just thinking about tomorrows programming. :) Good Night Github

---
+++ hybridcraft/FoodStuff/HybridModFoodStuff.java
@@ -0,0 +1,19 @@
+package hybridcraft.FoodStuff;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "HybridCraft 2", name = "HybridCraft 2.2 Food", version = "2.2")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
+public class HybridModFoodStuff {
+
+	@Init
+	public void load(FMLInitializationEvent event)
+	{
+		
+	}
+	
+}
