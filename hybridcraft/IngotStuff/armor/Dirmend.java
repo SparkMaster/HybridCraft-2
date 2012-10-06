7c49ea25862880f5153928a87bd347c963970b62
Sebastien Major <seb.major@live.com>
2012-10-06 05:20:43 -0400

    Even more cleanup

---
+++ hybridcraft/IngotStuff/armor/Dirmend.java
@@ -0,0 +1,30 @@
+package hybridcraft.IngotStuff.armor;
+
+import hybridcraft.IngotStuff.HybridModIngotStuff;
+import net.minecraft.src.EnumArmorMaterial;
+import net.minecraft.src.ItemArmor;
+import net.minecraft.src.ItemStack;
+import net.minecraftforge.common.IArmorTextureProvider;
+
+public class Dirmend extends ItemArmor implements IArmorTextureProvider{
+
+ public Dirmend(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
+ super(par1, par2EnumArmorMaterial, par3, par4);
+
+ }
+
+ public String getTextureFile(){
+ return "/hc/armor.png";
+
+ }
+
+ public String getArmorTextureFile(ItemStack par1){
+ if ( par1.itemID==HybridModIngotStuff.dirmendHelmet.shiftedIndex|| par1.itemID==HybridModIngotStuff.dirmendPlate.shiftedIndex||
+ par1.itemID==HybridModIngotStuff.dirmendBoots.shiftedIndex){
+ return "/hc/armor/dirmend_1.png";
+ }if(par1.itemID==HybridModIngotStuff.dirmendLegs.shiftedIndex){
+ return "/hc/armor/dirmend_2.png";
+ }return "/hc/armor/dirmend_2.png";
+ }
+
+}
---
+++ hybridcraft/IngotStuff/armor/Irold.java
@@ -0,0 +1,30 @@
+package hybridcraft.IngotStuff.armor;
+
+import hybridcraft.IngotStuff.HybridModIngotStuff;
+import net.minecraft.src.EnumArmorMaterial;
+import net.minecraft.src.ItemArmor;
+import net.minecraft.src.ItemStack;
+import net.minecraftforge.common.IArmorTextureProvider;
+
+public class Irold extends ItemArmor implements IArmorTextureProvider{
+
+ public Irold(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
+ super(par1, par2EnumArmorMaterial, par3, par4);
+
+ }
+
+ public String getTextureFile(){
+ return "/hc/armor.png";
+
+ }
+
+ public String getArmorTextureFile(ItemStack par1){
+ if ( par1.itemID==HybridModIngotStuff.iroldHelmet.shiftedIndex|| par1.itemID==HybridModIngotStuff.iroldPlate.shiftedIndex||
+ par1.itemID==HybridModIngotStuff.iroldBoots.shiftedIndex){
+ return "/hc/armor/irold_1.png";
+ }if(par1.itemID==HybridModIngotStuff.iroldLegs.shiftedIndex){
+ return "/hc/armor/irold_2.png";
+ }return "/hc/armor/irold_2.png";
+ }
+
+}
